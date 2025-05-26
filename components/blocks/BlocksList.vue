958daf3fa7735862e7d916b4237e9956cbcbc72b
Karen <karen.czock@bsbkoeln.de>
2025-05-26 09:56:46 +0200

    update

--- components/blocks/BlocksList.vue
+++ components/blocks/BlocksList.vue
@@ -1,10 +1,13 @@
 <template>
   <ul class="list-none space-y-xs" blocks-list>
     <li class="flex gap-sm" v-for="(item, index) in items" :key="index">
-      <BasicsText
-        :text="item.year"
-        class="block font-sans text-sm-sans text-right whitespace-nowrap w-40 shrink-0 tabular-nums tracking-tighter [&_span]:normal-nums [&_span]:tracking-normal"
-      />
+      <div class="w-40 shrink-0">
+        <BasicsText
+          :text="item.year"
+          class="block font-sans text-sm-sans text-right whitespace-nowrap tabular-nums tracking-tighter [&_span]:normal-nums [&_span]:tracking-normal"
+        />
+      </div>
+
       <BasicsText :text="item.text" />
     </li>
   </ul>
